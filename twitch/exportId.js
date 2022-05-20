384423b0962850c172aa146cd677ff4cad8101ca
jimmy durak <deeppaz@hotmail.com>
2022-05-20 22:45:47 +0300

    update

--- twitch/exportId.js
+++ twitch/exportId.js
@@ -1,5 +1,28 @@
 const fs = require("fs");
 const { data } = require("./data.json");
 
-var originalNoteString = (data[97].id).toString().split("\n");
-fs.writeFileSync("notes.txt", JSON.stringify(originalNoteString));
+let file_path = "./twitch/";
+let file_name = "clipIds.txt";
+
+// json
+// for (let i in data) {
+//   console.log(JSON.stringify(data[i].id));
+
+//   fs.appendFileSync(
+//     file_path + file_name,
+//     JSON.stringify(data[i].id, null, 4),
+//     function (err) {
+//       if (err) {
+//         console.log(err);
+//       } else {
+//         console.log("JSON saved!");
+//       }
+//     }
+//   );
+// }
+
+//txt
+for (let i = 0; i < data.length; i++) {
+  fs.appendFileSync(file_path + file_name, data[i].id + "," + "\n");
+  console.log(data[i].id + " is successfuly writed to file")
+}
